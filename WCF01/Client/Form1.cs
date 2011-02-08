ec42989139f76aacf9bb351c52082c209d4b771f
Jeffry Gonzalez <jeff@hypertheory.com>
2011-02-08 17:46:34 -0500

    Added Client

---
+++ WCF01/Client/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using CalculatorContracts;
+
+namespace Client
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var channel = new ChannelFactory<ICalculator>(
+				new NetNamedPipeBinding(NetNamedPipeSecurityMode.None),
+				new EndpointAddress("net.pipe://localhost/Calculator/CalculatorService"));
+
+			var client = channel.CreateChannel();
+			MessageBox.Show(client.Add("10", "20"));
+
+		}
+	}
+}
