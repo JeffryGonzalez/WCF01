eaa41b31b51be9693d58d70cc76a595a6f875147
Jeffry Gonzalez <jeff@hypertheory.com>
2011-02-08 19:39:23 -0500

    Added Client2 Using MEX-Proxy

---
+++ WCF01/Client2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var calculator = new svc.CalculatorClient();
+			MessageBox.Show(calculator.Add("50", "80"));
+		}
+	}
+}
